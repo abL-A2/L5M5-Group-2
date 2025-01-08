c53390f40d1b42c35d281226693f774b7b4f429e
AIgulKZNZ <167747718+AIgulKZNZ@users.noreply.github.com>
2025-01-08 20:29:27 +1300

    Added Directions Page functionality

--- Z-Energy/backend/controller.js
+++ Z-Energy/backend/controller.js
@@ -1,23 +1,16 @@
-const fetch = require("node-fetch");
+const address = "2 Queen Street, Auckland";
+const addressCoord = async (address)=>{
+    try{
+        const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.GOOGLE_API_KEY}`);
+        const data = await response.json();
+        if (data.status === "OK") {
+            const { lat, lng } = data.results[0].geometry.location;
+            return { lat, lng }; // Return the coordinates
+          } 
 
-const addressCoord = async (address) => {
-  try {
-    const response = await fetch(
-      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
-        address
-      )}&key=${process.env.GOOGLE_API_KEY}`
-    );
-    const data = await response.json();
-    if (data.status === "OK") {
-      const { lat, lng } = data.results[0].geometry.location;
-      return { lat, lng };
-    } else {
-      throw new Error("Address not found");
+    } catch (err) {
+        console.error("Geocoding Error: ", err);
     }
-  } catch (err) {
-    console.error("Geocoding Error: ", err);
-    throw err;
-  }
-};
+}
 
 module.exports = { addressCoord };
